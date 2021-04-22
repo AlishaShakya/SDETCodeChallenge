9ca1aa8aaf6a60b7a61c56173c352dd22f909cb7
Alisha Shakya <shakyaalisha245@gmail.com>
2021-04-22 23:57:14 +0545

    model created and default route configured

---
+++ SDETChallengeAddressGenerator/Controllers/RandomizerController.cs
@@ -0,0 +1,26 @@
+﻿using Bogus;
+
+using Microsoft.AspNetCore.Mvc;
+using SDETChallengeAddressGenerator.Models;
+using SDETChallengeAddressGenerator.UtilityFunctions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace SDETChallengeAddressGenerator.Controllers
+{
+    [Route("[controller]")]
+
+    public class RandomizerController : ControllerBase
+    {
+        [HttpGet("[action]")]
+        public IActionResult Address()
+        {
+            var result = RandomGenerator.AddressGenerator();
+
+            return Ok(result);
+        }
+    }
+}
